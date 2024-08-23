8ca69058df383552cd4e333f4a63afeaec247c64
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-08-23 18:18:47 +0700

    Q3

--- Training/Q3/src/Graph.java
+++ Training/Q3/src/Graph.java
@@ -5,146 +5,177 @@
 import java.io.*;
 import java.util.*;
 //-------------------------------------------------------------------------------
+
 public class Graph {
-  int [][] a; int n;
-  char v[];
-  int deg[];
-  Graph() {
-    v = "ABCDEFGHIJKLMNOP".toCharArray();
-    deg = new int[20];
-    a = new int[20][20];
-    n = 0;
-   }
 
-  void loadData(int k) {  //do not edit this function
-    RandomAccessFile f;int i,j,x;
-    String s;StringTokenizer t;
-    a = new int[20][20];
-    try {
-     f = new RandomAccessFile("data.txt","r");
-     for(i=0;i<k;i++) f.readLine();
-     s = f.readLine();s = s.trim();
-     n = Integer.parseInt(s);
-     for(i=0;i<n;i++) {
-       s = f.readLine();s = s.trim();
-       t = new StringTokenizer(s);
-       for(j=0;j<n;j++) { 
-         x = Integer.parseInt(t.nextToken().trim());
-         a[i][j] = x;
+    int[][] a;
+    int n;
+    char v[];
+    int deg[];
+
+    Graph() {
+        v = "ABCDEFGHIJKLMNOP".toCharArray();
+        deg = new int[20];
+        a = new int[20][20];
+        n = 0;
+    }
+
+    void loadData(int k) {  //do not edit this function
+        RandomAccessFile f;
+        int i, j, x;
+        String s;
+        StringTokenizer t;
+        a = new int[20][20];
+        try {
+            f = new RandomAccessFile("data.txt", "r");
+            for (i = 0; i < k; i++) {
+                f.readLine();
+            }
+            s = f.readLine();
+            s = s.trim();
+            n = Integer.parseInt(s);
+            for (i = 0; i < n; i++) {
+                s = f.readLine();
+                s = s.trim();
+                t = new StringTokenizer(s);
+                for (j = 0; j < n; j++) {
+                    x = Integer.parseInt(t.nextToken().trim());
+                    a[i][j] = x;
+                }
+            }
+            f.close();
+        } catch (Exception e) {
         }
-       }
-     f.close();
-     }
-    catch(Exception e) {}
 
-   }
+    }
 
-  void dispAdj() {
-    int i,j;
-    for(i=0;i<n;i++) {
-      System.out.println();
-      for(j=0;j<n;j++)
-        System.out.printf("%4d",a[i][j]);
-     }
-   }
-
-  void fvisit(int i, RandomAccessFile f) throws Exception {
-    f.writeBytes("  "+v[i]);
-   }
-
- void fdispAdj(RandomAccessFile f) throws Exception { 
-    int i,j;
-    f.writeBytes("n = "+n+"\r\n");
-    for(i=0;i<n;i++) {
-      f.writeBytes("\r\n");
-      for(j=0;j<n;j++)  f.writeBytes("  " + a[i][j]);
-     }
-    f.writeBytes("\r\n");
-   }
-
-  void breadth(boolean [] en, int i, RandomAccessFile f) throws Exception {
-    Queue q = new Queue();
-    int r,j;
-    q.enqueue(i); en[i]=true;
-    while(!q.isEmpty()) {
-      r = q.dequeue();
-      fvisit(r,f);
-      for(j=0;j<n;j++) {
-        if(!en[j] && a[r][j]>0) {
-         q.enqueue(j);en[j]=true;
+    void dispAdj() {
+        int i, j;
+        for (i = 0; i < n; i++) {
+            System.out.println();
+            for (j = 0; j < n; j++) {
+                System.out.printf("%4d", a[i][j]);
+            }
         }
-       }
-     }
-   }
+    }
 
-  void breadth(int  k, RandomAccessFile f) throws Exception {
-    boolean [] en = new boolean[20];
-    int i;
-    for(i=0;i<n;i++) en[i]=false;
-    breadth(en,k,f);
-    for(i=0;i<n;i++) 
-      if(!en[i]) breadth(en,i,f);
-   }
+    void fvisit(int i, RandomAccessFile f) throws Exception {
+        f.writeBytes("  " + v[i]);
+    }
 
- void depth(boolean [] visited,int k, RandomAccessFile f) throws Exception {
-    fvisit(k,f);visited[k]=true;
-    for(int i=0;i<n;i++) {
-      if(!visited[i] && a[k][i]>0) depth(visited,i,f);
-     }
-   }
-  void depth(int k, RandomAccessFile f) throws Exception {
-    boolean [] visited = new boolean[20];
-    int i;
-    for(i=0;i<n;i++) visited[i]=false;
-    depth(visited,k,f);
-    for(i=0;i<n;i++) 
-       if(!visited[i]) depth(visited,i,f);
-   }
+    void fdispAdj(RandomAccessFile f) throws Exception {
+        int i, j;
+        f.writeBytes("n = " + n + "\r\n");
+        for (i = 0; i < n; i++) {
+            f.writeBytes("\r\n");
+            for (j = 0; j < n; j++) {
+                f.writeBytes("  " + a[i][j]);
+            }
+        }
+        f.writeBytes("\r\n");
+    }
+
+    void breadth(boolean[] en, int i, RandomAccessFile f) throws Exception {
+        Queue q = new Queue();
+        int r, j;
+        q.enqueue(i);
+        en[i] = true;
+        while (!q.isEmpty()) {
+            r = q.dequeue();
+            fvisit(r, f);
+            for (j = 0; j < n; j++) {
+                if (!en[j] && a[r][j] > 0) {
+                    q.enqueue(j);
+                    en[j] = true;
+                }
+            }
+        }
+    }
+
+    void breadth(int k, RandomAccessFile f) throws Exception {
+        boolean[] en = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            en[i] = false;
+        }
+        breadth(en, k, f);
+        for (i = 0; i < n; i++) {
+            if (!en[i]) {
+                breadth(en, i, f);
+            }
+        }
+    }
+
+    void depth(boolean[] visited, int k, RandomAccessFile f) throws Exception {
+        fvisit(k, f);
+        visited[k] = true;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && a[k][i] > 0) {
+                depth(visited, i, f);
+            }
+        }
+    }
+
+    void depth(int k, RandomAccessFile f) throws Exception {
+        boolean[] visited = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            visited[i] = false;
+        }
+        depth(visited, k, f);
+        for (i = 0; i < n; i++) {
+            if (!visited[i]) {
+                depth(visited, i, f);
+            }
+        }
+    }
 
 //===========================================================================
 //(2)===YOU CAN EDIT OR EVEN ADD NEW FUNCTIONS IN THE FOLLOWING PART========
 //===========================================================================
 //====================================================================================
-  void f1() throws Exception {
-    loadData(1);
-    String fname = "f1.txt";
-    File g123 = new File(fname);
-    if(g123.exists()) g123.delete();
-    RandomAccessFile  f = new RandomAccessFile(fname, "rw"); 
-    breadth(2,f);
-    f.writeBytes("\r\n");
-    //-------------------------------------------------------------------------------------
-     /*You must keep statements pre-given in this function.
+    void f1() throws Exception {
+        loadData(1);
+        String fname = "f1.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        breadth(2, f);
+        f.writeBytes("\r\n");
+        //-------------------------------------------------------------------------------------
+        /*You must keep statements pre-given in this function.
        Your task is to insert statements here, just after this comment,
        to complete the question in the exam paper.*/
-      boolean[] en = new boolean[20];
-    Queue q = new Queue();
+        boolean[] en = new boolean[20];
+        Queue q = new Queue();
 
-    int r, j, count = 0; 
-    q.enqueue(2); 
-    en[2] = true;
+        int r, j, count = 0;
+        q.enqueue(2);
+        en[2] = true;
 
-    while (!q.isEmpty()) {
-        r = q.dequeue();
-        if (count >= 1 && count <= 6) {  // Start from 2nd, display up to 7th
-            fvisit(r, f);
-        }
-        count++;
-        for (j = 0; j < n; j++) {
-            if (!en[j] && a[r][j] > 0) {
-                q.enqueue(j);
-                en[j] = true;
+        while (!q.isEmpty()) {
+            r = q.dequeue();
+            if (count >= 1 && count <= 6) {  // Start from 2nd, display up to 7th
+                fvisit(r, f);
+            }
+            count++;
+            for (j = 0; j < n; j++) {
+                if (!en[j] && a[r][j] > 0) {
+                    q.enqueue(j);
+                    en[j] = true;
+                }
             }
         }
+
+        //-------------------------------------------------------------------------------------
+        f.writeBytes("\r\n");
+        f.close();
     }
 
-
-    //-------------------------------------------------------------------------------------
-    f.writeBytes("\r\n");
-    f.close();
-   }
-    void breadth2(boolean [] visited, int k, RandomAccessFile f) throws Exception {
-	 fvisit(k, f);
+    void breadth2(boolean[] visited, int k, RandomAccessFile f) throws Exception {
+        fvisit(k, f);
         visited[k] = true;
         for (int i = 1; i < 6; i++) {
             if (!visited[i] && a[k][i] > 0) {
@@ -152,10 +183,10 @@ public class Graph {
             }
         }
 
-   }
+    }
 
-  void breadth2(int  k, RandomAccessFile f) throws Exception{
-	 boolean[] visited = new boolean[20];
+    void breadth2(int k, RandomAccessFile f) throws Exception {
+        boolean[] visited = new boolean[20];
         int i;
         for (i = 1; i < 6; i++) {
             visited[i] = false;
@@ -167,29 +198,30 @@ public class Graph {
             }
         }
 
-   }
+    }
 
 //=================================================================================================
-  void f2() throws Exception {
-    loadData(12);
-    String fname = "f2.txt";
-    File g123 = new File(fname);
-    if(g123.exists()) g123.delete();
-    RandomAccessFile  f = new RandomAccessFile(fname, "rw"); 
-    f.writeBytes("\r\n");
-    //-------------------------------------------------------------------------------------
-     /*You must keep statements pre-given in this function.
+    void f2() throws Exception {
+        loadData(12);
+        String fname = "f2.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        f.writeBytes("\r\n");
+        //-------------------------------------------------------------------------------------
+        /*You must keep statements pre-given in this function.
        Your task is to insert statements here, just after this comment,
        to complete the question in the exam paper.*/
-      // You can use the statement fvisit(i,f); i = 0, 1, 2,...,n-1 to display the vertex i to file f2.txt 
-      //  and statement f.writeBytes(" " + k); to write  variable k to the file f2.txt  
-
-
-
-
-    //-------------------------------------------------------------------------------------
-    f.writeBytes("\r\n");
-    f.close();
-   }
+        // You can use the statement fvisit(i,f); i = 0, 1, 2,...,n-1 to display the vertex i to file f2.txt 
+        //  and statement f.writeBytes(" " + k); to write  variable k to the file f2.txt  
+        breadth2(n, f);
+        fvisit(n, f);
+        f.writeBytes(" " + n);
+        //-------------------------------------------------------------------------------------
+        f.writeBytes("\r\n");
+        f.close();
+    }
 
 }
